9b627a14e076d8109fd0dfc5c2f42b10427c8fcb
Jonathan Jethro <146849491+Xedrz@users.noreply.github.com>
2025-05-04 23:41:35 +0700

    Delete File/scan_ports.bat

--- File/scan_ports.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Skrip untuk pemindaian port
-set /p target_ip="Masukkan IP target untuk pemindaian port: "
-echo Memulai pemindaian port pada %target_ip%...
-nmap -p 1-65535 %target_ip% > results/%target_ip%_scan.txt
-echo Pemindaian port selesai! Hasil disimpan di results\%target_ip%_scan.txt
-pause
